33b08d4518ee26c25102a548a2f66ca467b2aae8
ReshmaMulla <sonureshma456@gmail.com>
2021-02-02 14:16:55 +0530

    Initial Commit

---
+++ SampleCICD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleCICD
+//
+//  Created by kartheek.p on 02/02/21.
+//  Copyright © 2021 Reshma.M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
